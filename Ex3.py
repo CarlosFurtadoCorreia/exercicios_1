aec5c362101f757ef868a093aeda3248d3dcede0
ciberseguranca <carlosfurtacorreia@enta.pt>
2022-09-27 08:36:00 +0000

    exs completados

--- Ex3.py
+++ Ex3.py
@@ -9,6 +9,14 @@ Mostre todos os números que satisfazem o pedido do utilizador
 import random
 
 
+def divisores(num):
+    zeros = 0
+    for j in range(1, num + 1):
+        if num % j == 0:
+            zeros += 1
+    return zeros
+
+
 def get_random(ini, fim):
     """
     Esta função devolve um número aleatório entre ini e fim inclusive
@@ -17,3 +25,36 @@ def get_random(ini, fim):
     :return: número aleatório
     """
     return random.randrange(ini, fim + 1)
+
+
+if __name__ == '__main__':
+    numeros = list()
+    inicio = int(input('Inicio: '))
+    fim = int(input('Fim: '))
+    ver = input('Ver apenas pares, impares ou primos? [1, 2, 3] ')
+
+    pares = list()
+    impares = list()
+    primos = list()
+
+    for n in range(inicio, fim + 1):
+        numeros.append(get_random(inicio, fim))
+
+    print(numeros)
+
+    for x in range(0, fim + 1):
+
+        if numeros[x] % 2 == 0:
+            pares.append(numeros[x])
+        else:
+            impares.append(numeros[x])
+
+        if divisores(numeros[x]) == 2:
+            primos.append(numeros[x])
+
+    if ver == '1':
+        print(f'Pares: {pares}')
+    if ver == '2':
+        print(f'Impares: {impares}')
+    if ver == '3':
+        print(f'Primos: {primos}')
